a0ff1f0acf0ec92c2843663fc451326be05b7874
EJ <hanej93@naver.com>
2023-02-12 20:15:21 +0900

    상수, 문자 더하기

--- src/test/java/study/querydsl/entity/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/entity/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl.entity;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -445,6 +446,37 @@ public class QuerydslBasicTest {
                         .otherwise("기타")
                 )
                 .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(
+                        member.username,
+                        Expressions.constant("A")
+                )
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        List<String> result = queryFactory
+                .select(
+                        // {username}_{age}
+                        member.username.concat("_").concat(member.age.stringValue())
+                )
+                .from(member)
+                .where(member.username.eq("member1"))
                 .fetch();
 
         for (String s : result) {
